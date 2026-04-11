8fa74ae962e9b01acd1f7dc87e0ac8ebc4769aa6
Couioly <3124546460@qq.com>
2026-04-11 09:53:37 +0800

    add .gitignore file

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-
+import uvicorn
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+uvicorn.run(app, host="0.0.0.0", port=8000)
